baba0f96bb423d81d2b8f41dca6e6fa27cd33d1e
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-10-17 20:45:16 -0400

    just finished with all that I can uploading to heroku now

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,7 +50,7 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.css$/i,
+          test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
         {
