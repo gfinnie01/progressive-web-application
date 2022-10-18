94ece401145ef59ee185bbb9e631cdf0d9ad5989
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-10-17 20:18:32 -0400

    trying to get my picture to show up so going to try a commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id,content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
